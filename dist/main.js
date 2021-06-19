2879bda3ca4faf2f97c0f3b5af47b722a805859c
Yann Klein <yann.klein@me.com>
2021-06-19 18:05:41 +0900

    built

--- dist/main.js
+++ dist/main.js
@@ -95,41 +95,50 @@
 
 const authorization = "Bearer sk_f9deb0090ac7688f94eb11ce09fd6a6e";
 
-// 1. Select all the elements eg input, button, username, email, bio, location
-const input = document.querySelector("#clearbitForm input");
-const form = document.querySelector("#clearbitForm");
-const username = document.querySelector("#userName");
-const email = document.querySelector("#userEmail");
-const location = document.querySelector("#userLocation");
-const bio = document.querySelector("#userBio");
+// 1 select input and submit button, and select name, email, bio, location -- Done
+const input  = document.getElementById('clearbitEmail');
+const submit  = document.getElementById('clearbitSubmit');
+const name  = document.getElementById('userName');
+const email  = document.getElementById('userEmail');
+const bio  = document.getElementById('userBio');
+const location  = document.getElementById('userLocation');
 
-const display = (data) => {
-  username.innerHTML = data.name.fullName;
-  email.innerHTML = data.email;
-  location.innerHTML = data.location;
-  bio.innerHTML = data.bio;
-  
+const displayInfo = (person) => {
+  // 5 add value to the name, email, bio, location
+  email.innerText = person.email;
+  name.innerText = person.name.fullName;
+  bio.innerText = person.bio;
+  location.innerText = person.location;
 }
 
-// 2. Listen to submit button
-form.addEventListener("submit", (event) => {
-  // 2.5 preventDefault()
-  event.preventDefault();
-  const url = `https://person.clearbit.com/v2/combined/find?email=${input.value}`;
-  // 3. Fetch API
+const fetchClearbit = (email) => {
+  const url = `https://person.clearbit.com/v2/combined/find?email=${email}`
   fetch(url, {
     headers: {
-      'Authorization': authorization
+      'Authorization': authorization,
+      'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:15.0) Gecko/20100101 Firefox/15.0.1'
     }
   })
-  .then(response => response.json())
-  .then((data) => {
-    // 4. Display
-    const personData = data.person;
-    console.log(personData);
-    display(personData);
-    })
-});
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data.person);
+      displayInfo(data.person);
+    });
+}
+
+const handleSubmit = (event) => {
+  // 2 get the input value
+  const emailToStalk = input.value;
+  // 4 preventDefault 
+  event.preventDefault();
+  console.log(event, emailToStalk);
+  // 4 get http request
+  fetchClearbit(emailToStalk);
+}
+
+// 3 addEventListener to the button
+submit.addEventListener("click", handleSubmit);
+
 
 /***/ })
 
